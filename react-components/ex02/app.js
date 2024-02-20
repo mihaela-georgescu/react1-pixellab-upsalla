842e637a658500173891e380f0a59f526074eb92
Mihaela Georgescu <mihaela.georgescu50@gmail.com>
2024-02-20 14:58:17 +0200

    react-components

--- react-components/ex02/app.js
+++ react-components/ex02/app.js
@@ -57,3 +57,164 @@ const message1 = person.skills.reduce((message1, skill, index, skills) => {
   return (message1 += `${skill}${punctuation}`);
 }, '');
 console.log(message1);
+
+console.warn(`
+ In mod similar, afiseaza skill-urile care NU incep cu j.
+`);
+const message2 = person.skills.reduce((filteredSkills, skill) => {
+  if (skill.toLowerCase().startsWith('j')) {
+    return filteredSkills;
+  }
+
+  filteredSkills.push(skill);
+
+  return filteredSkills;
+}, []);
+console.log(message2.join(', '));
+
+console.warn(`
+ Folosind reduce afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."
+`);
+const message3 = person.friends.reduce((message, friend, index, friends) => {
+  const { name, surname } = friend;
+  const punctuation = friends.length - 1 === index ? '.' : ', ';
+
+  message += `${name} ${surname} ${punctuation}`;
+
+  return message;
+}, 'Prietenii mei se numesc ');
+console.log(message3);
+
+console.warn(`
+ Folosind reduce, afiseaza numarul total de ani pe care il au persoanele din arrayul friends,
+ doar daca varsta este mai mare sau egala cu 30 de ani.
+`);
+console.log(
+  person.friends.reduce((totalAge, { age }) => {
+    if (age >= 30) {
+      totalAge += age;
+    }
+
+    return totalAge;
+  }, 0),
+);
+
+console.warn(`
+ Folosind reduce, afiseaza suma anilor de nastere a persoanelor.
+`);
+console.log(
+  person.friends.reduce((sumYears, friend) => {
+    const { age } = friend;
+    const birthYear = new Date().getFullYear() - age;
+
+    return (sumYears += birthYear);
+  }, 0),
+);
+
+console.warn(`
+ Afiseaza fraza: "Intre Dragos si Larry este o diferenta de xx ani. Intre Dragos si Steven... ",
+  doar daca varsta prietenului este impara.
+`);
+const finalSentence = person.friends.reduce((sentence, friend, index) => {
+  const { name, age } = friend;
+
+  if (age % 2 === 0) {
+    return sentence;
+  }
+
+  const ageDiff = Math.abs(person.age - age);
+
+  sentence += `Intre ${person.name} si ${name} este o diferenta de ${ageDiff} ani. `;
+
+  return sentence;
+}, '');
+console.log(finalSentence.trim());
+
+console.warn(`
+Folosind obiectul person si reduce, afiseaza in consola un string care contine skillurile
+persoanei, separate prin virgula
+`);
+const skillsMessage = person.skills.reduce((message, skill, index, skills) => {
+  const punctuation = skills.length - 1 === index ? '' : ', ';
+  return (message += `${skill}${punctuation}`);
+}, '');
+
+console.log(skillsMessage);
+
+console.warn(`
+ In mod similar, afiseaza skillurile care incep cu c
+`);
+const cSkills = person.skills.reduce((message, skill, index, skills) => {
+  if (!skill.toLowerCase().startsWith('c')) {
+    return message;
+  }
+  const punctuation = skills.length - 1 === index ? '' : ', ';
+  return (message += `${skill}${punctuation}`);
+}, '');
+
+console.log(cSkills);
+
+console.warn(`
+ Folosind reduce, afiseaza propozitia: "Numele de familie ale prietenilor mei sunt: xxx, xxx , xxx."
+`);
+const friendList = person.friends.reduce((message, friend, index, friends) => {
+  const { surname } = friend;
+  const punctuation = friends.length - 1 === index ? '.' : ', ';
+  message += `${surname}${punctuation}`;
+
+  return message;
+}, 'Numele de familie ale prietenilor mei sunt: ');
+
+console.log(friendList);
+
+console.warn(`
+ Folosind reduce, afiseaza numarul total de ani pe care il au persoanele din arrayul friends
+`);
+const total = person.friends.reduce((message, friend, index, friends) => {
+  const { age } = friend;
+  return (message += age);
+}, 0);
+
+console.log(total);
+
+console.warn(`
+ Folosind reduce, afiseaza suma anilor persoanelor.
+`);
+const personBirthYear = new Date().getFullYear() - person.age;
+
+const totalBirthYears = person.friends.reduce(
+  (message, friend, index, friends) => {
+    const { age } = friend;
+    const birthYear = new Date().getFullYear() - age;
+    return (message += birthYear);
+  },
+  0,
+);
+
+console.log(personBirthYear + totalBirthYears);
+
+console.warn(`
+ Afiseaza diferenta de varsta dintre persoana si prietenii din arrayul friends.
+`);
+const ageDifference = person.friends.reduce(
+  (message, { age }, index, friends) => {
+    const difference = Math.abs(person.age - age);
+    return (message += `${difference} `);
+  },
+  '',
+);
+console.log(ageDifference);
+
+console.warn(`
+ Afiseaza fraza: "Intre Dragos si Larry este o diferenta de xx ani. Intre Dragos si Steven... ". Repeta pentru tot arrayul friends.
+`);
+const ageCompared = person.friends.reduce(
+  (message, { name, age }, index, friends) => {
+    const ageDifference = Math.abs(person.age - age);
+    message += `Intre ${person.name} si ${name} este o diferenta de ${ageDifference} ani. `;
+
+    return message;
+  },
+  '',
+);
+console.log(ageCompared);
--- react-components/ex04/app.js
+++ react-components/ex04/app.js
@@ -27,5 +27,109 @@ const person = {
   },
 };
 
+console.warn(`
+Folosind Object.entries() pe proprietatea skills, afiseaza abilitatile
+ persoanei daca acestea sunt true. Folosind propozitii de forma:
+ “person.name cunoaste: html.” “person.name cunoaste: javaScript.”`);
+const entries1 = Object.entries(person.skills);
+entries1.forEach((skillEntry) => {
+  const [skillId, isKnown] = skillEntry;
 
+  console.log(
+    `${person.name} ${isKnown ? 'cunoaste' : 'nu cunoaste'} ${skillId}.`,
+  );
+});
 
+console.warn(`
+  Prin aceeasi metoda, afiseaza o lista inversata cu numele complet
+  inversat al prietenilor.
+`);
+const reversedFriendsList = Object.entries(person.friends).reverse();
+reversedFriendsList.forEach((friendEntry) => {
+  const [, friend] = friendEntry;
+  const { surname, name } = friend;
+
+  console.log(`${surname} ${name}`);
+});
+
+console.warn(`
+  Afiseaza propozitia: “Prietenii mei sunt Larry, Steven si Carol.”
+  folosind Object.entries()
+`);
+console.log(
+  Object.entries(person.friends).reduce(
+    (message, friendEntry, index, friends) => {
+      const length = friends.length;
+      const [, { name }] = friendEntry;
+      const punctuation =
+        length - 1 === index ? '.' : length - 2 === index ? ' si ' : ', ';
+
+      message += `${name}${punctuation}`;
+      return message;
+    },
+    'Prietenii mei sunt ',
+  ),
+);
+
+console.warn(`
+In mod similar afiseaza mai multe propozitii (cate una per console.log()) care
+sa afiseze: “Diferenta de varsta intre Larry si Dragos este de xxx ani.” etc…
+`);
+const entries2 = Object.entries(person.friends);
+entries2.forEach((friendEntry) => {
+  const [, friend] = friendEntry;
+  const { name, age } = friend;
+  const ageDiff = Math.abs(age - person.age);
+  console.log(
+    `Diferenta de varsta intre ${name} si ${person.name} este ${ageDiff}`,
+  );
+});
+
+console.warn(`
+Folosind Object.entries() pe proprietatea skills, afiseaza toate abilitatile din obiectul skills.
+`);
+Object.entries(person.skills).forEach((skillEntry) => {
+  const [skill] = skillEntry;
+  console.log(skill);
+});
+
+console.warn(`
+Prin aceeasi metoda, afiseaza o lista cu numele complet al prietenilor.
+`);
+Object.entries(person.friends).forEach((friendEntry) => {
+  const [, friend] = friendEntry;
+  const { name, surname } = friend;
+
+  console.log(`${name} ${surname}`);
+});
+
+console.warn(`
+Afiseaza propozitia: “Prietenii mei sunt Larry Larryson, Steven Stevenson si Carol Carolson.”
+folosind Object.entries()
+`);
+console.log(
+  Object.entries(person.friends).reduce(
+    (message, friendEntry, index, friends) => {
+      const [, friend] = friendEntry;
+      const { name, surname } = friend;
+      const punctuation = friends.length - 1 === index ? '.' : ', ';
+
+      return (message += `${name} ${surname}${punctuation}`);
+    },
+    'Prietenii mei sunt ',
+  ),
+);
+
+console.warn(`
+In mod similar, afiseaza mai multe propozitii (cate una per console.log()) care sa afiseze:
+“Larry are xx ani. Steven are …”
+`);
+const entries3 = Object.entries(person.friends).forEach(
+  (friendEntry, index, friends) => {
+    const [, { name, age }] = friendEntry;
+    message = `${name} are ${age} ani.`;
+
+    console.log(message);
+  },
+  '',
+);
